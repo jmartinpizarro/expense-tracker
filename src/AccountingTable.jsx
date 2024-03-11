d15e88c1ea0a7aa5df5f590b1b4759071afefd22
Javier Martin <jmartinpizarro04@gmail.com>
2024-03-11 18:07:04 +0100

    Done main design.

--- src/AccountingTable.jsx
+++ src/AccountingTable.jsx
@@ -1,24 +1,24 @@
 export default function AccountingTable(){
 
     return(
-    <div className="h-72 bg-white rounded-2xl">
-        <h2>Historic - Recently</h2>
-        <ul>
+    <div className="h-72 bg-white rounded-2xl flex flex-col">
+        <h2 className="mx-6 mt-4">Historic - Recently</h2>
+        <ul className="mx-6 h-3/5">
           <li>element1</li>
+          <li>element2</li>
         </ul>
 
-        <div>
+        <div className="flex flex-row gap-20 items-center justify-center">
           <div>
             <h3>Total earned:</h3>
-            <span>---€</span>
+            <span className="block w-full text-right">---€</span>
           </div>
 
           <div>
             <h3>Total spent:</h3>
-            <span>---€</span>
+            <span className="block w-full text-right">---€</span>
           </div>
         </div>
-      </div>
-    )
-
+    </div>
+    )  
 }
