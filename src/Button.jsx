57dcd4ccbbd6874e0034855b67880c66ad62ad64
Javier Martin <jmartinpizarro04@gmail.com>
2024-03-12 18:10:38 +0100

    Finish button component

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,22 +1,23 @@
-import { useEffect } from "react"
 import { useState } from "react"
 
-export default function Button(){
+export default function Button() {
+  const [isRotated, setIsRotated] = useState(false);
 
-    const [turned, setTurned] = useState(false)
+  const handleClick = () => {
+    setIsRotated(!isRotated);
+  };
 
-    const handleClick = () => {
-        setTurned(!turned)
-        const itemToRotate = document.getElementById('cross-img')
-        if (turned == true) {
-            itemToRotate.style.rotate = '45deg'
-        }
-        
-    }
-
-    return(
-        <div className="absolute rounded-full self-center -bottom-5 bg-paleOrange w-12 h-12 flex items-center justify-center cursor-pointer hover:scale-125 transition ease-in-out duration-700" onClick={handleClick}>
-          <img className="w-4/6 h-4/6" src="./plus.png" alt="For adding an expense, click here" id="cross-img"/>
-        </div>
-    )
+  return (
+    <div
+      className="absolute rounded-full self-center -bottom-5 bg-paleOrange w-12 h-12 flex items-center justify-center cursor-pointer hover:scale-125 transition ease-in-out duration-700"
+      onClick={handleClick}
+    >
+      <img
+        className={`w-4/6 h-4/6 ${isRotated ? 'rotate-45' : ''} transition ease-in-out duration-400`} // Apply rotation conditionally
+        src="./plus.png"
+        alt="For adding an expense, click here"
+        id="cross-img"
+      />
+    </div>
+  );
 }
