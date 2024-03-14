b71434d848bacb22e8e462bee99ca31752e6284d
Javier Martin <jmartinpizarro04@gmail.com>
2024-03-14 12:19:01 +0100

    Not ideal but maybe a possible solution.

--- src/AccountingTable.jsx
+++ src/AccountingTable.jsx
@@ -3,9 +3,11 @@ import { useState } from "react"
 export default function AccountingTable(){
     const [isRotated, setIsRotated] = useState(false);
 
+
     const handleClick = () => {
       setIsRotated(!isRotated);
     };
+
 
     return(
     <div className="h-3/5 bg-white rounded-2xl flex flex-col relative">
@@ -27,9 +29,20 @@ export default function AccountingTable(){
           </div>
         </div>
 
-        <div className={`flex bg-paleBlue absolute bottom-0 w-full h-2/4 rounded-lg transition duration-400 ${isRotated ? 'opacity-100' : 'opacity-0'}`}>
-          <h2 className="text-center mx-auto my-2 font-semibold h-5">Add new expense</h2>
-        </div>
+        <section className={`flex flex-col gap-10 bg-paleBlue absolute bottom-0 w-full h-2/4 rounded-lg transition duration-400 ${isRotated ? 'opacity-100' : 'opacity-0'}`}>
+          <h2 className="text-center mx-auto mt-2 font-semibold h-5">Add new expense</h2>
+          <form className="flex flex-row mx-auto gap-5">
+            <div>
+              <input type="radio" id="income" name="income" value="income"></input>
+              <label htmlFor="income">Income</label>
+            </div>
+
+            <div>
+              <input type="radio" id="expense" name="expense" value="expense"></input>
+              <label htmlFor="expense">Expense</label>
+            </div>
+          </form>
+        </section>
 
         <div className="absolute rounded-full self-center -bottom-5 bg-paleOrange w-12 h-12 flex items-center justify-center cursor-pointer hover:scale-125 transition ease-in-out duration-700 z-10" onClick={handleClick}>
         <img
